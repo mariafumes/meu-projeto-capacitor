48619efdc8b919e99b0fbfd6ee6a3cd408ba9715
mariafumes <mariadasilva0307@gmail.com>
2025-06-25 11:17:44 -0400

    oi

--- src/routes/cassino/sorteio/+page.svelte
+++ src/routes/cassino/sorteio/+page.svelte
@@ -12,9 +12,30 @@ function sortear(){
     }
 }
 </script>
+<style>
+     .styled-button5 {
+    width: 260px;
+    max-width: 100%;
+    padding: 16px;
+    margin: 12px 0;
+    font-size: 18px;
+    color: white;
+    background-color: #797d93;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    transition: transform 0.2s, background-color 0.3s ease;
+
+    }
+
+</style>
 <div class="container text-center mt-5">
-    <button class="btn btn-secondary" onclick={sortear}>Sortear números</button>
+    <h3>Sorteando números para Mega-Sena</h3>
+    <br/>
+    <button class="styled-button5" onclick={sortear}><h1>🎰</h1></button>
     <br/>
     <br/>
-    {sorteados}
+    <h5>Os números sorteados são:</h5>
+    <br/>
+    <h3>{sorteados}</h3>
 </div>
